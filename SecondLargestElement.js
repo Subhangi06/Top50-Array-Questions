7bbbf6d3de6b9cec7ad31bf144bbcf7c38aac1db
Subhangi Agarwal <subhangi@Subhangis-MacBook-Pro.local>
2025-02-03 12:04:24 +0530

    first commit

---
+++ SecondLargestElement.js
@@ -0,0 +1,12 @@
+// Given an array of positive integers arr[] of size n,
+//  the task is to find second largest distinct element in the array.
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 there is no second largest element.
