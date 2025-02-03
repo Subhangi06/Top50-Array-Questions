a2fbf052f87ed8fbff58214432a84b7cbe5bddd3
Subhangi Agarwal <subhangi@Subhangis-MacBook-Pro.local>
2025-02-03 12:50:47 +0530

    Second largest element in an array

--- SecondLargestElement.js
+++ SecondLargestElement.js
@@ -10,3 +10,17 @@
 // Input: arr[] = [10, 10, 10]
 // Output: -1
 // Explanation: The largest element of the array is 10 there is no second largest element.
+
+function SecondLargestElement(arr) {
+  let largest = -1,
+    secondLargest = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) largest = arr[i];
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > secondLargest && arr[i] !== largest) secondLargest = arr[i];
+  }
+  return secondLargest;
+}
+
+console.log(SecondLargestElement([12, 35, 1, 10, 34, 1]));
